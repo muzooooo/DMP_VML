2c4953e66e5ac677402f84414af127744acc88ba
Sangsta Musonda <sangstamusonda@gmail.com>
2025-07-30 14:55:00 +0200

    Moved home.html to dmp_project/templates/dmp_project/ and updated view

--- dmp_project/views.py
+++ dmp_project/views.py
@@ -6,4 +6,4 @@ def home(request):
     Renders the public home page for the Vangarde Music Limited site.
     This template extends base.html for consistent site layout.
     """
-    return render(request, 'home.html')
+    return render(request, 'dmp_project/home.html')
